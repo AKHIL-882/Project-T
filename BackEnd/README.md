6f7f415119239a5e1d5fe2be0ef3162d99f4264f
Duggirala Akhil <akhil.duggirala@loqbox.com>
2024-05-02 07:26:46 +0530

    Passport SetUp

--- BackEnd/README.md
+++ BackEnd/README.md
@@ -2,3 +2,6 @@
 
 # Technologies Used:
 ## Laravel
+
+
+#02-05-2024: SetUp Passport for Authentication 
--- BackEnd/app/Providers/AuthServiceProvider.php
+++ BackEnd/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,15 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\V1\User;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Laravel\Passport\AuthCode;
+use Laravel\Passport\Client;
+use Laravel\Passport\PersonalAccessClient;
+
+// use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -12,8 +19,14 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array<class-string, class-string>
      */
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
+        //  User::class => UserPolicy::class,
     ];
 
     /**
@@ -24,7 +37,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::useClientModel(Client::class);
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
+        Passport::tokensExpireIn(now()->addDays(config('passport.tokens_expire_in')));
+        Passport::refreshTokensExpireIn(now()->addDays(config('passport.personal_access_tokens_expire_in')));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(config('passport.refresh_tokens_expire_in')));
+        Passport::tokensCan(config('passport.scopes'));
 
-        //
     }
 }
